eb57bb5d6892f818bc516fddfb3196fc0a4501e0
Caio Ramos <aramoscaio@gmail.com>
2023-04-01 21:19:10 -0300

    Added validations to the get and post requests to show error messages more specifically

--- app.py
+++ app.py
@@ -28,6 +28,14 @@ def get_item_in_store(item_id):  # sourcery skip: use-next
 @app.post("/store")
 def create_store():
     store_data = request.get_json()
+
+    if "name" not in store_data:
+        abort(400, message="Bad request. Ensure 'name' is included in the JSON payload")
+    
+    for store in stores.values():
+        if store_data["name"] == store["name"]:
+            abort(400, message="Store already exists.")
+            
     store_id = uuid.uuid4().hex
     store = {**store_data, "id": store_id}
     stores[store_id] = store
@@ -38,8 +46,23 @@ def create_store():
 def create_item(name):
     item_data = request.get_json()
     
-    if item_data["store_id"] not in stores:
-        return {"message": "Store not found"}, 404
+    if (
+        "price" not in item_data 
+        or "store_id" not in item_data
+        or "name" not in item_data
+    ):
+        abort(
+            400,
+            message="Bad request. Ensure 'price', 'store_id' and 'name' are included in the JSON payload"
+    
+        )
+
+    for item in items.values():
+        if(
+            item_data["name"] == item["name"]
+            and item_data["store_id"] == item["store_id"]
+        ):
+            abort(400, message="Item already exists.")
 
     item_id = uuid.uuid4().hex
     item = {**item_data, "id": item_id}
