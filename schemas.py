6a7f7e20cf77fd6847fe3811db06372ca323e81b
Caio Ramos <aramoscaio@gmail.com>
2023-04-04 23:44:11 -0300

    Added ItemSchema

--- schemas.py
+++ schemas.py
@@ -1,5 +1,11 @@
 from marshmallow import Schema, fields
 
+class ItemSchema(Schema):
+    id = fields.Str(dump_only=True)
+    name = fields.Str(required=True)
+    price = fields.Float(required=True)
+    store_id = fields.Str(required=True)
+
 class ItemUpdateSchema(Schema):
     name = fields.Str()
     price = fields.Float()
