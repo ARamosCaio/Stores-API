153149bb8922d4ceb249fcfd4204aa23d66319af
Caio Ramos <aramoscaio@gmail.com>
2023-04-07 23:13:50 -0300

    Changed schemas to avoid infinite nesting

--- schemas.py
+++ schemas.py
@@ -1,15 +1,21 @@
 from marshmallow import Schema, fields
 
-class ItemSchema(Schema):
+class PlainItemSchema(Schema):
     id = fields.Str(dump_only=True)
     name = fields.Str(required=True)
     price = fields.Float(required=True)
-    store_id = fields.Str(required=True)
+
+class PlainStoreSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str()
+
+class ItemSchema(PlainItemSchema):
+    store_id = fields.Int(required=True, load_only=True)
+    store = fields.Nested(PlainItemSchema(), dump_only=True)
 
 class ItemUpdateSchema(Schema):
     name = fields.Str()
     price = fields.Float()
 
-class StoreSchema(Schema):
-    id = fields.Str(dump_only=True)
-    name = fields.Str(required=True)
+class StoresSchema(PlainStoreSchema):
+    items = fields.List(field.Nested(PlainItemSchema()),dump_only=True)
