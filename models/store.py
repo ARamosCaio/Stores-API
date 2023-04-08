f2668b692738f0b7060d943527ed7901f6209955
Caio Ramos <aramoscaio@gmail.com>
2023-04-07 22:41:28 -0300

    Added one to many relationship between item and stores

--- models/store.py
+++ models/store.py
@@ -5,3 +5,5 @@ class StoreModel(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(40), unique=True, nullable=False)
+
+    itema = db.relationship("ItemModel", back_populates="store", lazy="dynamic")
