7019858e59b725f2686263db50973e0aef49c573
Caio Ramos <aramoscaio@gmail.com>
2023-04-08 01:25:58 -0300

    added models insert using sqlalchemy

--- resources/item.py
+++ resources/item.py
@@ -2,8 +2,10 @@ import uuid
 from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
-from db import items
 from schemas import ItemSchema, ItemUpdateSchema
+from sqlalchemy.exc import SQLAlchemyError
+from db import db
+from models import ItemModel
 
 blp = Blueprint("Items", "items", description="Operations on items")
 
@@ -44,23 +46,15 @@ def get(self):
 @blp.arguments(ItemSchema)
 @blp.response(201, ItemSchema)
 def post(self, item_data):
-    for item in items.values():
-        if (
-            item_data["name"] == item["name"]
-            and item_data["store_id"] == item["store_id"]
-        ):
-            abort(400, message="Item already exists.")
+    
+    item = ItemModel(**item_data)
 
-    for item in items.values():
-        if(
-            item_data["name"] == item["name"]
-            and item_data["store_id"] == item["store_id"]
-        ):
-            abort(400, message="Item already exists")
-
-    item_id =uuid.uuid4().hex
-    item = {**item_data, "id": item_id}
-    items[item_id] = item
+    try:
+        db.session.add(item)
+        de.session.commit()
+    
+    except SQLAlchemyError:
+        abort(500, message="An error occurred while inserting the item")
 
     return item
 
