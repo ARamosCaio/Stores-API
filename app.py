a654e9f040d18ba50747fa9dcfb639d0295e4baf
Caio Ramos <aramoscaio@gmail.com>
2023-04-10 19:49:06 -0300

    Imported an registered tag blueprint

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_smorest import Api
 
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
-
+from resources.tag import blp as TagBlueprint
 from db import db
 import models
 
@@ -17,7 +17,7 @@ def create_app():
     app.config["OPENAPI_URL_PREFIX"] = "/"
     app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
     app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
-    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
 
@@ -28,6 +28,7 @@ def create_app():
 
     api.register_blueprint(ItemBlueprint)
     api.register_blueprint(StoreBlueprint)
+    api.register_blueprint(TagBlueprint)
 
     return app
         
