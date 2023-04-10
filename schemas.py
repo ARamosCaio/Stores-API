2fc9a86507ee745a1f2e1cef2e279a107660aed5
Caio Ramos <aramoscaio@gmail.com>
2023-04-09 22:27:02 -0300

    Added Tag schema

--- schemas.py
+++ schemas.py
@@ -9,6 +9,10 @@ class PlainStoreSchema(Schema):
     id = fields.Int(dump_only=True)
     name = fields.Str()
 
+class PlainTagSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str()
+
 class ItemSchema(PlainItemSchema):
     store_id = fields.Int(required=True, load_only=True)
     store = fields.Nested(PlainItemSchema(), dump_only=True)
@@ -17,5 +21,10 @@ class ItemUpdateSchema(Schema):
     name = fields.Str()
     price = fields.Float()
 
-class StoresSchema(PlainStoreSchema):
+class StoreSchema(PlainStoreSchema):
     items = fields.List(field.Nested(PlainItemSchema()),dump_only=True)
+    tag = fields.List(fields.Nested(PlainTagSchema()), dump_only=True)
+
+class TagSchema(PlainTagSchema):
+    store_id = fields.Int(load_only=True)
+    store = fields.Nested(PlainStoreSchema(), dump_only=True)
