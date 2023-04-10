e23c64c739f9c317b6627065e36e75ef455c2393
Caio Ramos <aramoscaio@gmail.com>
2023-04-10 20:54:11 -0300

    Fixed tag.py import and blueprint name

--- resources/tag.py
+++ resources/tag.py
@@ -1,12 +1,12 @@
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
-from sqlalchemy_exc import SQLAlchemyError
+from sqlalchemy.exc import SQLAlchemyError
 
 from db import db
 from models import TagModel, StoreModel
 from schemas import TagSchema
 
-blp = Blueprint("Tags", "tags", description="Operations on tags")
+blp = Blueprint("tags", "tags", description="Operations on tags")
 
 @blp.route("/store/<string:store_id>/tag")
 class TagsInStore(MethodView):
