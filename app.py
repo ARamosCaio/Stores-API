9e829e39a1c428633cc95f177642f640cc677d1e
Caio Ramos <aramoscaio@gmail.com>
2023-04-13 19:55:27 -0300

    Added usser blueprint to app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_smorest import Api
 from flask_jwt_extended import JWTManager
 
+from resources.user import blp as UserBlueprint
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
 from resources.tag import blp as TagBlueprint
@@ -28,6 +29,7 @@ def create_app():
     with app.app_context():
         db.create_all()
 
+    api.register_blueprint(UserBlueprint)
     api.register_blueprint(ItemBlueprint)
     api.register_blueprint(StoreBlueprint)
     api.register_blueprint(TagBlueprint)
