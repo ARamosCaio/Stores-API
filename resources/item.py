802c512e27d83ccfaaa2ad9a0696bc20ff98b084
Caio Ramos <aramoscaio@gmail.com>
2023-04-16 22:53:22 -0300

    Added token verification to delete an item

--- resources/item.py
+++ resources/item.py
@@ -21,6 +21,10 @@ class Item(MethodView):
 
     @jwt_required()
     def delete(self, item_id):
+        jwt = get_jwt()
+        if not jwt.get("is_admin"):
+            abort(401, message="Admin privilege required")
+        
         item = ItemModel.query.get_or_404(item_id)
         db.session.delete(item)
         db.session.commit()
