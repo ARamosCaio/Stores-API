df5083dadbbec4b804d87a56a7b5349d0abd6a7c
Caio Ramos <aramoscaio@gmail.com>
2023-04-18 21:49:44 -0300

    feat: Added token refreshing

--- resources/user.py
+++ resources/user.py
@@ -1,7 +1,7 @@
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from passlib.hash import pbkdf2_sha256
-from flask_jwt_extended import create_access_token, get_jwt, jwt_required
+from flask_jwt_extended import create_access_token, get_jwt, jwt_required, create_refresh_token, get_jwt_identity
 
 from db import db
 from models import UserModel
@@ -57,7 +57,8 @@ class UserLogin(MethodView):
         ).first
 
         if user and pbkdf2_sha256.verify(user_data["password"], user.password):
-            acess_token = create_access_token(identity=user.id)
-            return {"acess_token": acess_token}, 200
+            acess_token = create_access_token(identity=user.id, fresh=True)
+            refresh_token = create_refresh_token(user.id)
+            return {"acess_token": acess_token, "refresh_token": refresh_token}, 200
         
         abort(401, message="Invalid credentials")
